4bc02cea704c400b49806f82ac3e6b20304c3802
Jorge Flores <a01254623@tec.mx>
2025-03-18 19:41:32 -0700

    Version 2

---
+++ Programa.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stlib.h>
+#include <math.h>
+
+int main() {
+
+	int lenght;
+	scanf("%i", %lenght);
+	
+	int office = pow(2,lenght);
+	printf("%i",office);
+	return 0;
+}
